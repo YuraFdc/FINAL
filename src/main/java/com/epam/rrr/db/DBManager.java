99ca4dfaf0809e2900b9c478e38251f057dac25b
Yurii Fedorchak <fedorchak.yu@gmail.com>
2021-09-01 15:13:23 +0300

    3

--- src/main/java/com/epam/rrr/db/DBManager.java
+++ src/main/java/com/epam/rrr/db/DBManager.java
@@ -1,4 +1,36 @@
 package com.epam.rrr.db;
 
 public class DBManager {
+    private static String connectionUrl;
+    private static DBManager instance;
+
+    public static void setConnectionUrl(String connectionUrl) {
+        DBManager.connectionUrl = connectionUrl;
+    }
+
+    private DBManager() {
+        try(FileReader reader = new FileReader("app.properties");) {
+            Properties p = new Properties();
+            p.load(reader);
+            setConnectionUrl(p.getProperty("connection.url"));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static synchronized DBManager getInstance() {
+        if (instance == null)
+            instance = new DBManager();
+        return instance;
+    }
+
+    public Connection getConnection(String connectionUrl) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(connectionUrl);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return connection;
+    }
 }
